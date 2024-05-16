2ada6294e8eedf2d623b826812a42cef13e398ac
Tim Wolstenholme <timwolstenholme06@gmail.com>
2024-05-16 23:18:33 +0100

    changed the structure of the code and added the start of a tax object

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
-class Investment:
+class InterestProduct:
     def __init__(self, holding, interest):
-        self.__holding = holding
+        self.__amount = holding
         self.__interest = interest
 
     def __add__(self, other):
-        other = self.__charge_stamp_duty(other)
-        self.__holding += other
+        self.__amount += other
 
     def __sub__(self, other):
-        self.__holding -= other
+        self.__amount -= other
 
-    def get_holding(self):
-        return self.__holding
+    def get_amount(self):
+        return self.__amount
 
     def annual_interest(self):
-        self.__holding *= self.__interest
+        self.__amount *= self.__interest
 
-    def __charge_stamp_duty(self,amount):
+class Investment(InterestProduct):
+    def __init__(self, holding, interest):
+        super().__init__(holding, interest)
+
+    def __add__(self, other):
+        other = self.__charge_stamp_duty(other)
+        self.__amount += other
+
+    def __charge_stamp_duty(self, amount):
         return amount*0.995
+
+class Debt(InterestProduct):
+    def __init__(self, amount_owed, interest):
+        super().__init__(amount_owed, interest)
+
+class Tax:
+    def __init__(self, thresholds:dict):
+        self.__thresholds = thresholds
+
+    def charge_tax(self, amount):
+        cur_threshold = -1
+        if amount < min(list(self.__thresholds.keys())):
+            return amount
+        for threshold in list(self.__thresholds.keys()):
+            if amount < threshold:
+                break
+            cur_threshold = threshold
+        tax_rate = self.__thresholds[cur_threshold]
+        return amount*(1-tax_rate)
 
 class ISA:
     def __init__(self, isaID):
@@ -33,7 +59,7 @@ class ISA:
         if amount > self.__total_holdings:
             return "Not enough money in the ISA"
         for name in list(self.__holdings.keys()):
-            self.__holdings[name] - round(self.__holdings[name].get_holding / amount,2)
+            self.__holdings[name] - round(self.__holdings[name].get_amount / amount, 2)
         self.__total_holdings -= amount
 
     def remove_specific(self,amount, holding):
@@ -47,10 +73,10 @@ class ISA:
         proportions = []
         amounts = []
         for holding in list(self.__holdings.keys()):
-            amount = holding.get_holding()
+            amount = holding.get_amount()
             amounts.append(amount)
             proportions.append(round(amount/self.__total_holdings,4))
         return [[self.__holdings.keys()[i],amounts[i],proportions[i]] for i in range(len(self.__holdings))]
 
     def get_investment_holding(self,holding):
-        return self.__holdings[holding].get_holding()
+        return self.__holdings[holding].get_amount()
